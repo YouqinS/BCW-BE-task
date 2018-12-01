3c156b3b9b8b7e894faf20662d47c15b488c9e96
YouqinS <33859677+YouqinS@users.noreply.github.com>
2018-12-01 16:43:49 +0200

    added authentication

--- app.js
+++ app.js
@@ -1,25 +1,91 @@
 'use strict';
 require('dotenv').config();
+
 const express = require('express');
 const db = require('./modules/database');
 const resize = require('./modules/resize');
 const exif = require('./modules/exif');
 const bodyParser = require('body-parser');
 const jsonParser = bodyParser.json();
-
+const connection = db.connect();
 const multer = require('multer');
 const upload = multer({dest: 'public/uploads/'});
+const fs         = require('fs');
+const https      = require('https');
+const http       = require('http');
+const passport   = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const session = require('express-session');
 
 const app = express();
 app.use(bodyParser.json());
-const connection = db.connect();
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+
+passport.serializeUser((user, done) => {
+    console.log('serialize: ' + user);
+    done(null, user);
+});
+
+passport.deserializeUser((user, done) => {
+    done(null, user);
+});
+
+app.use(session({
+    secret: 'keyboard LOL cat',
+    resave: true,
+    saveUninitialized: true,
+    cookie: { secure: true }
+}));
+
+passport.use(new LocalStrategy(
+    (username, password, done) => {
+        console.log('Here we go: ' + username);
+        if (username !== process.env.USR_NAME || password !== process.env.USR_PWD) { return done(null, false); }
+        return done(null, { username: username } );
+    }
+));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+app.post('/login',
+    passport.authenticate('local', { successRedirect: '/node/', failureRedirect: '/node/login.html' }));
+
+app.set('trust proxy');
+const sslkey  = fs.readFileSync('/etc/pki/tls/private/ca.key');
+const sslcert = fs.readFileSync('/etc/pki/tls/certs/ca.crt');
+const options = {
+    key: sslkey,
+    cert: sslcert
+};
+
+app.get('/', (req,res) => {
+    if (req.secure) {
+        console.log(req.user);
+        if(req.user !== undefined) res.send('Hello ' + req.user.username);
+        else res.send('https :)');
+    }
+    else res.send('hello not secure?');
+});
+
+//app.listen(8000);
+http.createServer((req, res) => {
+    const redir = 'https://' + req.headers.host + '/node' + req.url;
+    console.log(redir);
+    res.writeHead(301, { 'Location': redir });
+    res.end();
+}).listen(8000);
+
 
 const cb = (result, res) => {
     //console.log(result);
     res.send(result);
 };
 
-app.use(express.static('public'));
 
 
 //update
@@ -127,4 +193,6 @@ app.get('/images', (req, res) => {
 });
 
 
-app.listen(3000);
+https.createServer(options, app).listen(3000);
+
+//app.listen(3000);
